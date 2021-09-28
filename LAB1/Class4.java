be45876ab3bfd7504a5d5c27408709e729889642
Ironhide1234 <ironhide610@gmail.com>
2021-09-28 03:32:54 +0300

    New vers

--- LAB1/Class4.java
+++ LAB1/Class4.java
@@ -1,13 +1,20 @@
 public class Class4{
     public static void main(String[] args){
-        int[] b = new int[6];
-        for(int i = 1; i <= 4; i++){
-            System.out.print(b[i] + i);
+        int[] b  = {5, 3, 2, 6};
+        int[] copy = new int[4];    
+        for(int i = 0; i < b.length; i++){
+            System.out.print(b[i]);
         }
         System.out.print("\n");
-        for(int i = 4; i > 0; i--){
-            b[i + 1] = b[i];
-            System.out.print(b[i] + i);
+        for(int i = 0; i < 4; i++){
+            if (i == 0){
+                copy[i] = b[i+3];
+            } else {
+                copy[i] = b[i-1];
+            }
         }
+        int number = copy[0]*1000 + copy[1]*100 + copy[2]*10 + copy[3];
+        System.out.printf("Now number is: %d\n", number);
+        System.out.printf("Log is %.1f\n", Math.log(number));
     }
 }
